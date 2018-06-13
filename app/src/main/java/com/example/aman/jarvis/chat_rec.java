01f3ea59afa775013b4174bebfb494a3ba0f2143
engamankumar <engamankumar@gmail.com>
2018-06-13 17:50:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/aman/jarvis/chat_rec.java
@@ -0,0 +1,25 @@
+package com.example.aman.jarvis;
+
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+
+
+public class chat_rec extends RecyclerView.ViewHolder  {
+
+
+
+    TextView leftText,rightText;
+
+    public chat_rec(View itemView){
+        super(itemView);
+
+        leftText = (TextView)itemView.findViewById(R.id.leftText);
+        rightText = (TextView)itemView.findViewById(R.id.rightText);
+        
+
+
+    }
+}
